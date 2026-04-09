af47894a25266e6f2f47f05280ed450f23a12724
Gushenp <gustavohenriquep861programer@gmail.com>
2026-04-09 19:42:51 -0300

    Adição da classe: Pessoa

--- src/cadastrodepessoas/Pessoa.java
+++ src/cadastrodepessoas/Pessoa.java
@@ -9,6 +9,40 @@ package cadastrodepessoas;
  * @author 1076313
  */
 public class Pessoa {
+    // ==== Organização de variáveis ====
     private String nome, CPF;
     private int idade;
+    
+    // ==== Construtor ====
+    public Pessoa(String nome, String CPF, int idade) {
+        this.nome = nome;
+        this.CPF = CPF;
+        this.idade = idade;
+    }
+
+    // ==== Setters ====
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    
+    public void setCPF(String CPF) {
+        this.CPF = CPF;
+    }
+    
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+    
+    // ==== Gueters ==== 
+    public String getNome() {
+        return nome;
+    }
+
+    public String getCPF() {
+        return CPF;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
 }
