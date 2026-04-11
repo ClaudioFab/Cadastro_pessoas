84beeebd9d5c364afa3f13d17c751cd6f198c6e9
ClaudioFab <cla.rob.fabiano@gmail.com>
2026-04-11 10:31:20 -0300

    Atualização e modificações semi-finais

--- src/cadastrodepessoas/Main_Cadastro.java
+++ src/cadastrodepessoas/Main_Cadastro.java
@@ -1,92 +1,96 @@
 package cadastrodepessoas;
 
 import java.util.ArrayList;
-import java.util.Scanner;
 import javax.swing.JOptionPane;
 
 public class Main_Cadastro {
 
     public static void main(String[] args) {
+        Pessoa.apresentar();
         menu();
-
     }
-
+    
     public static void menu() {
         ArrayList<Pessoa> cadastros = new ArrayList<>();
         int escolha = 0;
-
         do {
-//            System.out.println("========Menu========");
-//            System.out.println("1. Cadastrar Pessoa");
-//            System.out.println("2. Consultar Cadastros");
-//            System.out.println("3. Finalizar");
             escolha = Integer.parseInt(JOptionPane.showInputDialog(null, "1 - Cadastrar Pessoa\n\n2 - Consultar Cadastros\n\n3 - Finalizar\n\nEscolha a opção desejada.\n\n", "Menu", JOptionPane.QUESTION_MESSAGE));
-
             switch (escolha) {
                 case 1:
-                    cadastros.add(cadastrarPessoa());
-
                     Pessoa p = cadastrarPessoa();
                     if (p.getIdade() == 0) {
-                        System.out.println("Pessoa não cadastrada! Tente novamente!");
+                        System.out.println("Pessoa não cadastrada!");
+                        JOptionPane.showMessageDialog(null,"Pessoa não cadastrada!","Erro!",JOptionPane.ERROR_MESSAGE);
                     } else {
                         cadastros.add(p);
+                        Pessoa.porQuantidade();
+                        System.out.println("Cadastro realizado com Sucesso!");
+                        JOptionPane.showMessageDialog(null,"Cadastro realizado com Sucesso!","Novo Cadastrado",JOptionPane.WARNING_MESSAGE);
                     }
                     break;
                 case 2:
                     mostrarCadastros(cadastros);
                     break;
                 case 3:
-                    System.out.println("Finalizando sistema!");
-                    JOptionPane.showMessageDialog(null, "Finalizando sistema!", "Encerrando", JOptionPane.INFORMATION_MESSAGE);
+                    System.out.println("Programa finalizado");
+                    JOptionPane.showMessageDialog(null, "Programa finalizado.","Encerrando", JOptionPane.INFORMATION_MESSAGE);
                     System.exit(0);
-                    break;
+                    break;    
                 default:
                     System.out.println("Opção invalida! Retornando ao menu!");
                     JOptionPane.showMessageDialog(null, "Opção invalida!\n\nRetornando ao menu!", "Erro", JOptionPane.ERROR_MESSAGE);
             }
-
-        } while (escolha != 3);
-
+        } while (true);
     }
 
     public static Pessoa cadastrarPessoa() {
-
         System.out.println("Digite o nome: ");
         String nome = JOptionPane.showInputDialog(null, "Digite o nome: ", "Nome", JOptionPane.QUESTION_MESSAGE);
 
         System.out.println("Digite a idade: ");
         int idade = Integer.parseInt(JOptionPane.showInputDialog(null, "Digite a idade: ", "Idade", JOptionPane.QUESTION_MESSAGE));
-
+        
         System.out.println("Digite o CPF: ");
         String cpf = JOptionPane.showInputDialog(null, "Digite o CPF: ", "CPF", JOptionPane.QUESTION_MESSAGE);
-
-        return new Pessoa(nome, cpf, idade);
-
+        
+        return new Pessoa(nome, cpf, idade, 0);
     }
-
     public static void mostrarCadastros(ArrayList<Pessoa> cadastros) {
         if (cadastros.isEmpty()) {
             System.out.println("Lista vazia.");
             JOptionPane.showMessageDialog(null, "Lista vazia.", "Vazio", JOptionPane.WARNING_MESSAGE);
+            
         } else {
-            System.out.println("bleu");
+            System.out.println("Consulta de Cadastros");
+            Object[] itens = {"Mostrar lista de Cadastros","Pesquisar por nome","Total de Cadastrados","Voltar"};
+            Object opcao = JOptionPane.showInputDialog(null,"Escolha a opção desejada.","Consulta de Cadastros",JOptionPane.QUESTION_MESSAGE,null,itens,itens[0]);
+        
+            if (opcao.toString().equals("Mostrar lista de Cadastros")) {
+                System.out.println("lista de Cadastros");
+                for (Pessoa p8 : cadastros) {
+                    p8.listarInformacoes();  
+                }
+            
+            }else if(opcao.toString().equals("Pesquisar por nome")){
+                System.out.println("Pesquisa por nome");
+                
 
-            int escolha = Integer.parseInt(JOptionPane.showInputDialog(null, "1 - Mostrar lista\n\n2 - Pesquisar por nome\n\nEscolha a opção desejada.\n\n", "Menu", JOptionPane.QUESTION_MESSAGE));
 
-            switch (escolha) {
-                case 1:
-                    for (Pessoa p8 : cadastros) {
-                        p8.listarInformacoes();
-                    }
-                    break;
-                case 2:
-                    System.out.println("S");
-                    break;
-                default:
-                    System.out.println("Opção Invalida!");
+                //CONTINUAR AQUI A FASE DE PESQUISAR NOME!
+
+
+
+            }else if(opcao.toString().equals("Total de Cadastrados")){
+                System.out.println("Foram cadastrados até o momento um total de "+Pessoa.getQuantidade()+" pessoas.");
+                JOptionPane.showMessageDialog(null,"Foram cadastrados até o momento um total de "+Pessoa.getQuantidade()+" pessoas.","Total de Cadastros",JOptionPane.INFORMATION_MESSAGE);
+                
+            }else if(opcao.toString().equals("Voltar")){
+                System.out.println("Voltar");
+                return;
             }
+                
 
         }
     }
+
 }
--- src/cadastrodepessoas/Pessoa.java
+++ src/cadastrodepessoas/Pessoa.java
@@ -3,22 +3,20 @@ package cadastrodepessoas;
 
 import javax.swing.JOptionPane;
 
-
 public class Pessoa {
     // === Importar funções ===
     
     // ==== Organização de variáveis ====
     private String nome, CPF;
     private int idade;
-    private int quantidade;
+    private static int quantidade;
     
     // ==== Construtor ====
-    public Pessoa(String nome, String CPF, int idade) {
+    public Pessoa(String nome,String CPF,int idade,int quantidade) {
         this.nome = nome;
         this.CPF = CPF;
         //Confere se a idade não está negativa
         conferirIdade(idade);
-        this.quantidade = quantidade;
     }
 
     // ==== Setters ====
@@ -34,6 +32,10 @@ public class Pessoa {
         conferirIdade(idade);
     }
     
+    public void setQuantidade(int quantidade){
+        this.quantidade = quantidade;  
+    }
+    
     // ==== Gueters ==== 
     public String getNome() {
         return nome;
@@ -45,6 +47,10 @@ public class Pessoa {
 
     public int getIdade() {
         return idade;
+    }
+    
+    public static int getQuantidade() {
+        return quantidade;
     }
 
 // ===== Funções usuário ====
@@ -63,8 +69,15 @@ public class Pessoa {
         JOptionPane.showMessageDialog(null,"Nome: "+this.nome+"\nIdade: "+this.idade+"\nCPF: "+this.CPF,"Cadastrados",JOptionPane.INFORMATION_MESSAGE);
         
     }
-    
-    public void quantidade(){
-        this.quantidade++;
+
+    public static void porQuantidade() {
+        quantidade++;
     }
+    
+    public static void apresentar(){
+        System.out.println("Atividade em Grupo Git (Cadastro de Pessoas)");
+        //Atividade em Grupo Git - Cadastro de Pessoas
+        JOptionPane.showMessageDialog(null,"Atividade em Grupo Git\n(Cadastro de Pessoas)\n\nParticipantes:\n\n* Gustavo Arvelos → Classe Pessoa (Funções Extras + Lógica)\n\n* Rokston Damasio → Main (Menu e Interação + Lógica)\n\n* Cláudio Fabiano → Testes (Melhorias e Firulas)","Iniciando",JOptionPane.INFORMATION_MESSAGE);
+    }
+
 }
